2aa4d93d1d762555ff0b68cb797759794786cad6
AleksanderGondek <gondekaleksander@hotmail.com>
2018-01-13 18:55:53 +0100

    Initial commit with working solution

---
+++ Dialogs/RootDialog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+using gipher.Giphy;
+
+namespace gipher.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            var botHandle = ConfigurationManager.AppSettings["BotId"];
+            var giphyApi = new GiphyApi(ConfigurationManager.AppSettings["GiphyApiToken"]);
+
+            var requesterName = activity.From.Name;
+            var queryText = activity.RemoveMentionText(botHandle);
+            var imageUrl = await giphyApi.GetImageUrlFromText(queryText);
+
+            var responseText = $"{requesterName} searched for '{queryText}' \n\n {imageUrl}";
+            await context.PostAsync(responseText);
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ Giphy/GiphyApi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Flurl;
+using Flurl.Http;
+
+namespace gipher.Giphy
+{
+    public class GiphyApi
+    {
+        private const string GiphyBaseApiUrl = "http://api.giphy.com";
+        private readonly string _apiKey;
+
+        public GiphyApi(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public async Task<string> GetImageUrlFromText(string text)
+        {
+            var url = "";
+            try
+            {
+                var request = GetGiphySearchRequest(text);
+                var response = await request.GetJsonAsync();
+                url = response.data.images.original.url;
+            }
+            catch (Exception error)
+            {
+                url = error.Message;
+            }
+
+            return url;
+        }
+
+        private Url GetGiphySearchRequest(string text)
+        {
+            return new Url(GiphyBaseApiUrl)
+                .AppendPathSegments(new List<string>() {"v1", "gifs", "translate"})
+                .SetQueryParams(new {api_key = _apiKey, s = text});
+        }
+    }
+}
